79f66f3f3ad042d28b85294d5294bc46efd29efd
LPatton4 <77860909+LPatton4@users.noreply.github.com>
2021-04-20 22:38:04 -0500

    successfully added map with markers

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var map;
+ 
+function initMap(){
+	map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.8781, lng: -87.6298},
+    zoom: 10
+  });
+	
+	const rtds = new google.maps.Marker ({
+		animation: google.maps.Animation.DROP,
+		position: {lat: 41.845460, lng: -87.622810},
+		map,
+		title: "Russell Talbert Dance Studio",
+	});
+	
+	const jones = new google.maps.Marker ({
+		animation: google.maps.Animation.DROP,
+		position: {lat: 41.872898, lng: -87.627930},
+		map,
+		title: "Jones College Prep",
+	});
+	
+	const fullerpark = new google.maps.Marker ({
+		animation: google.maps.Animation.DROP,
+		position: {lat: 41.811770, lng: -87.634600},
+		map,
+		title: "Fuller Park",
+	});
+}
+	
